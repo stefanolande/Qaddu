26387fa40b491bc8f49a73b6ab63be70cb942e5d
simone porcu <simociccio2@hotmail.it>
2016-01-28 16:34:48 +0100

    added roba

---
+++ app/src/main/java/so2/unica/qaddu/Helpers/DatabaseHelper.java
@@ -0,0 +1,118 @@
+package so2.unica.qaddu.helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import so2.unica.qaddu.models.WorkoutItem;
+import so2.unica.qaddu.models.WorkoutPoint;
+
+
+/**
+ * Created by Sergio
+ */
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    private static DatabaseHelper mDatabaseHelper;
+    private static final String DATABASE_NAME = "quaddu.db";
+    private static final int DATABASE_VERSION = 9;
+
+    private DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // Trick to call onCreate - onUpgrade
+        getWritableDatabase();
+    }
+
+    public static void initialize(Context ctx){
+        if(mDatabaseHelper == null){
+            mDatabaseHelper = new DatabaseHelper(ctx.getApplicationContext());
+        }
+    }
+
+    public static DatabaseHelper getIstance(){
+        return mDatabaseHelper;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource) {
+        Log.i(DatabaseHelper.class.getName(), "onCreate");
+        try {
+            TableUtils.createTable(connectionSource, WorkoutItem.class);
+            TableUtils.createTable(connectionSource, WorkoutPoint.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource, int i, int i1) {
+        Log.i(DatabaseHelper.class.getName(), "onUpgrade");
+        try {
+            TableUtils.dropTable(connectionSource, WorkoutItem.class, true);
+            TableUtils.dropTable(connectionSource, WorkoutPoint.class, true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        onCreate(sqLiteDatabase, connectionSource);
+    }
+
+    public <K> void addData(K object, Class<K> name) {
+        try {
+            Dao dao = getDao(name);
+            dao.create(object);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public <K> void DeleteAll(Class<K> name){
+        try {
+            TableUtils.clearTable(getConnectionSource(), name);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public <K> List<K> GetData(Class<K> name)
+    {
+        List<K> list = null;
+        try {
+            Dao dao = getDao(name);
+            list = dao.queryForAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+    public <K> Object getItemById(int id, Class<K> name) {
+        Object item = null;
+        try {
+            Dao dao = getDao(name);
+            item = dao.queryForId(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    public Dao<WorkoutItem,Integer> getDao(){
+        try {
+            return getDao(WorkoutItem.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/so2/unica/qaddu/Models/WorkoutPoint.java
@@ -0,0 +1,112 @@
+package so2.unica.qaddu.models;
+
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by stefano on 27/01/16.
+ * This class models an istant of the workout
+ */
+
+@DatabaseTable(tableName = "workout_points")
+public class WorkoutPoint {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField
+    private double latitude;
+    @DatabaseField
+    private double longitude;
+    @DatabaseField
+    private double speed;
+    @DatabaseField
+    private double step;
+    @DatabaseField
+    private double altitude;
+    @DatabaseField
+    private long time;
+    @DatabaseField
+    private double distance;
+
+    @DatabaseField(foreign = true)
+    public transient WorkoutItem workout;
+
+    public WorkoutItem getWorkout() {
+        return workout;
+    }
+
+    public void setWorkout(WorkoutItem workout) {
+        this.workout = workout;
+    }
+
+    public WorkoutPoint(){};
+
+    public WorkoutPoint(WorkoutItem workout, double latitude, double longitude, double speed, double altitude, long time, double distance) {
+        this.workout = workout;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.speed = speed;
+        this.altitude = altitude;
+        this.time = time;
+        this.distance = distance;
+
+        //calculate the step from the speed
+        double km = distance / 1000;
+        double stepInSeconds = time / km;
+        this.step = stepInSeconds / 60;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getStep() {
+        return step;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
