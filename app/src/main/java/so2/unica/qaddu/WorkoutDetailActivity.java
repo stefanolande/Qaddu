432077e447f89fa4de257bafa59ed45c53fd3ec1
Stefano Lande <landeste@gmail.com>
2016-02-11 15:50:23 +0100

    interactive graph

--- app/src/main/java/so2/unica/qaddu/WorkoutDetailActivity.java
+++ app/src/main/java/so2/unica/qaddu/WorkoutDetailActivity.java
@@ -299,6 +299,8 @@ public class WorkoutDetailActivity extends AppCompatActivity {
          graph.getViewport().setMinX(workoutPoints.get(0).getTime() * 1000);
          graph.getViewport().setMaxX(workoutPoints.get(workoutPoints.size() - 1).getTime() * 1000);
          graph.getViewport().setXAxisBoundsManual(true);
+         graph.getViewport().setScalable(true);
+         graph.getViewport().setScrollable(true);
          graph.invalidate();
 
       } else if (xAxis == xAxisType.DISTANCE) {
@@ -323,6 +325,8 @@ public class WorkoutDetailActivity extends AppCompatActivity {
          graph.getViewport().setMinX(workoutPoints.get(0).getDistance() / 1000.0);
          graph.getViewport().setMaxX(workoutPoints.get(workoutPoints.size() - 1).getDistance() / 1000.0);
          graph.getViewport().setXAxisBoundsManual(true);
+         graph.getViewport().setScalable(true);
+         graph.getViewport().setScrollable(true);
          graph.invalidate();
 
       }
