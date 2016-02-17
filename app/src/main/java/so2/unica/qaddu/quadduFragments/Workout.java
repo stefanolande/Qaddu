35eee96fd0cff34593f703ba5a640d829263d7c5
Stefano Lande <landeste@gmail.com>
2016-02-17 16:22:13 +0100

    WorkoutService at 50%

--- app/src/main/java/so2/unica/qaddu/quadduFragments/Workout.java
+++ app/src/main/java/so2/unica/qaddu/quadduFragments/Workout.java
@@ -4,11 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -32,198 +31,195 @@ import so2.unica.qaddu.services.WorkoutService;
 
 public class Workout extends Fragment {
 
-    @Bind(R.id.circle_container)
-    LinearLayout mCircleContainer;
-
-    @Bind(R.id.min_circle)
-    View mCircle;
-
-    @Bind(R.id.tv_instant_speed)
-    TextView tvInstantSpeed;
-
-    @Bind(R.id.tv_target)
-    TextView tvTargetSpeed;
-
-    @Bind(R.id.tv_total_km)
-    TextView tvTotalKm;
-
-    @Bind(R.id.tv_total_speed)
-    TextView tvTotalKmH;
-
-    @Bind(R.id.tv_total_time)
-    TextView tvTotalTime;
-
-    @Bind(R.id.tv_total_step)
-    TextView tvTotalStep;
-
-    @Bind(R.id.tv_last_x_speed)
-    TextView tvLastSpeed;
-
-    @Bind(R.id.tv_last_x_step)
-    TextView tvLastStep;
-
-    @Bind(R.id.nameWorkout)
-    EditText etNameWorkout;
-
-    @Bind(R.id.btn_stop)
-    ImageButton bStop;
-
-    @Bind(R.id.btn_start)
-    ImageButton bStart;
-
-    BroadcastReceiver mBroadcastReceiver;
-
-
-    int mContainterWidth;
-    int tmpOff = -100;
-
-    float totalKm = 0;      //It contains the total kilometers traveled
-    float totalKmH = 0;     //It contains the total speed
-    float totalTime = 0;    //It contain the time of the workout
-    float targetSpeed = 0;  //It contain the target speed
-    float lastKmh = 0;      //It contains the speed of the last X meter
-    float totalStep = 0;    //It contain the total step
-    float lastStep = 0;     //It contain the step of the last X meter
-    float instantSpeed = 0; //It contain the instant speed
-    String nameWorkout = "";//It contain the workout's name
+   BroadcastReceiver mBroadcastReceiver;
+   int mContainterWidth;
+   int tmpOff = -100;
+   float totalKm = 0;      //It contains the total kilometers traveled
+   float totalKmH = 0;     //It contains the total speed
+   float totalTime = 0;    //It contain the time of the workout
+   float targetSpeed = 0;  //It contain the target speed
+   float lastKmh = 0;      //It contains the speed of the last X meter
+   float totalStep = 0;    //It contain the total step
+   float lastStep = 0;     //It contain the step of the last X meter
+   float instantSpeed = 0; //It contain the instant speed
+   String nameWorkout = "";//It contain the workout's name
+   @Bind(R.id.circle_container)
+   private LinearLayout mCircleContainer;
+   @Bind(R.id.min_circle)
+   private View mCircle;
+   @Bind(R.id.tv_instant_speed)
+   private TextView tvInstantSpeed;
+   @Bind(R.id.tv_target)
+   private TextView tvTargetSpeed;
+   @Bind(R.id.tv_total_km)
+   private TextView tvTotalKm;
+   @Bind(R.id.tv_total_speed)
+   private TextView tvTotalKmH;
+   @Bind(R.id.tv_total_time)
+   private TextView tvTotalTime;
+   @Bind(R.id.tv_total_step)
+   private TextView tvTotalStep;
+   @Bind(R.id.tv_last_x_speed)
+   private TextView tvLastSpeed;
+   @Bind(R.id.tv_last_x_step)
+   private TextView tvLastStep;
+   @Bind(R.id.nameWorkout)
+   private EditText etNameWorkout;
+   @Bind(R.id.btn_stop)
+   private ImageButton bStop;
+   @Bind(R.id.btn_start)
+   private ImageButton bStart;
+   private String mWorkoutName = "Untitled workout";
+   private boolean mWorkoutRunning = false;
+   private boolean mWorkoutPaused = false;
 
 
-    public Workout() {
-        // Required empty public constructor
-    }
+   public Workout() {
+      // Required empty public constructor
+   }
 
-    private void setCircleOffset(double o) {
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) mCircle.getLayoutParams();
-        int margin = (int) (mContainterWidth / 100 * o);
-        params.setMargins(margin, 0, 0, 0); //substitute parameters for left, top, right, bottom
-        mCircle.setLayoutParams(params);
-    }
+   private void setCircleOffset(double o) {
+      LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) mCircle.getLayoutParams();
+      int margin = (int) (mContainterWidth / 100 * o);
+      params.setMargins(margin, 0, 0, 0); //substitute parameters for left, top, right, bottom
+      mCircle.setLayoutParams(params);
+   }
 
 
-    //This method is used to set the speed into the TextView of the instant speed
-    private void setInstantSpeed(double instantSpeed, boolean isCalculated) {
-        DecimalFormat df = new DecimalFormat("#0.00");
-        tvInstantSpeed.setText(df.format(instantSpeed) + " KM/H");
-        if (isCalculated) {
-            tvInstantSpeed.setTextColor(ContextCompat.getColor(getActivity(), R.color.QadduRed));
-        } else {
-            tvInstantSpeed.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
-        }
-    }
+   //This method is used to set the speed into the TextView of the instant speed
+   private void setInstantSpeed(double instantSpeed, boolean isCalculated) {
+      DecimalFormat df = new DecimalFormat("#0.00");
+      tvInstantSpeed.setText(df.format(instantSpeed) + " KM/H");
+      if (isCalculated) {
+         tvInstantSpeed.setTextColor(ContextCompat.getColor(getActivity(), R.color.QadduRed));
+      } else {
+         tvInstantSpeed.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+      }
+   }
 
-    //This method is used to set the target speed into the TextView of the target speed
-    private void setTargetSpeed(float targetSpeed) {
-        tvTargetSpeed.setText(Float.toString(targetSpeed) + " Km/h");
-    }
+   //This method is used to set the target speed into the TextView of the target speed
+   private void setTargetSpeed(float targetSpeed) {
+      tvTargetSpeed.setText(Float.toString(targetSpeed) + " Km/h");
+   }
 
-    //This method is used to set the total Km traveled into the TextView of the total km
-    private void setTotalKm(float totalKm) {
-        tvTotalKm.setText(Float.toString(totalKm) + " Km");
-    }
+   //This method is used to set the total Km traveled into the TextView of the total km
+   private void setTotalKm(float totalKm) {
+      tvTotalKm.setText(Float.toString(totalKm) + " Km");
+   }
 
-    //This method is used to set the total speed into the TextView of the total speed
-    private void setTotalSpeed(double totalKmH) {
-        tvTotalKmH.setText(Double.toString(totalKmH) + " KM/H");
-    }
+   //This method is used to set the total speed into the TextView of the total speed
+   private void setTotalSpeed(double totalKmH) {
+      tvTotalKmH.setText(Double.toString(totalKmH) + " KM/H");
+   }
 
-    //This method is used to set the total time into the TextView of the total time
-    private void setTotalTime(float totalTime) {
-        //tvTotalTime.setText(Float.toString(totalTime));
-        tvTotalTime.setText("01:31:12");
-    }
+   //This method is used to set the total time into the TextView of the total time
+   private void setTotalTime(float totalTime) {
+      //tvTotalTime.setText(Float.toString(totalTime));
+      tvTotalTime.setText("01:31:12");
+   }
 
-    //This method is used to set the total step speed into the TextView of the total step
-    private void setTotalStep(float totalStep) {
-        tvTotalStep.setText(Float.toString(totalStep) + " /KM");
-    }
+   //This method is used to set the total step speed into the TextView of the total step
+   private void setTotalStep(float totalStep) {
+      tvTotalStep.setText(Float.toString(totalStep) + " /KM");
+   }
 
-    //This method is used to set the last X meter speed into the TextView of the last X meter speed
-    private void setLastSpeed(float lastSpeed) {
-        tvLastSpeed.setText(Float.toString(lastSpeed) + " KM/H");
-    }
+   //This method is used to set the last X meter speed into the TextView of the last X meter speed
+   private void setLastSpeed(float lastSpeed) {
+      tvLastSpeed.setText(Float.toString(lastSpeed) + " KM/H");
+   }
 
-    //This method is used to set the last X meter's step speed into the TextView of the LastStep
-    private void setLastStep(float lastStep) {
-        tvLastStep.setText(Float.toString(lastStep) + " /KM");
-    }
+   //This method is used to set the last X meter's step speed into the TextView of the LastStep
+   private void setLastStep(float lastStep) {
+      tvLastStep.setText(Float.toString(lastStep) + " /KM");
+   }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
 
-    }
+   }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_workout, container, false);
-        ButterKnife.bind(this, view);
+   @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      View view = inflater.inflate(R.layout.fragment_workout, container, false);
+      ButterKnife.bind(this, view);
 
-        bStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!GPSService.running) {
-                    Intent intent = new Intent(getActivity().getApplicationContext(), GPSService.class);
-                    getActivity().startService(intent);
-                    //when the workout is play or pause, the user can stop the workout (the stop button in enable, is blu)
-                    bStop.setImageDrawable(getResources().getDrawable(R.drawable.ic_stop));
-                    bStart.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause));
-                } else {
-                    Intent intent = new Intent(getActivity().getApplicationContext(), GPSService.class);
-                    getActivity().stopService(intent);
-
-                    bStart.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
-                    //bStop.setImageDrawable(getResources().getDrawable(R.drawable.ic_stop));
-                }
-
-                if (!WorkoutService.running) {
-                    Intent intent = new Intent(getActivity().getApplicationContext(), WorkoutService.class);
-                    intent.putExtra(WorkoutService.WORKOUT_TITLE, etNameWorkout.getText().toString());
-                    getActivity().startService(intent);
-                }
+      bStart.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if (!mWorkoutRunning || mWorkoutPaused) {
+               //when the workout is play or pause, the user can stop the workout (the stop button in enable, is blu)
+               bStop.setImageDrawable(getResources().getDrawable(R.drawable.ic_stop));
+               bStart.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause));
+               mWorkoutRunning = true;
+            } else {
+               bStart.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
+               mWorkoutPaused = true;
             }
-        });
 
-        bStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (GPSService.running) {
-                    Intent intent = new Intent(getActivity().getApplicationContext(), GPSService.class);
-                    getActivity().stopService(intent);
-                    bStart.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
-                }
-                Intent intent = new Intent(getActivity().getApplicationContext(), WorkoutService.class);
-                getActivity().stopService(intent);
-                //when the Workout is stopped, the user can't click stop button again
-                bStop.setImageDrawable(getResources().getDrawable(R.drawable.ic_stopgray));
+            if (!mWorkoutRunning) {
+               Intent intent = new Intent(getActivity().getApplicationContext(), WorkoutService.class);
+
+               if (!etNameWorkout.getText().equals("")) {
+                  mWorkoutName = etNameWorkout.getText().toString();
+               } else {
+                  etNameWorkout.setText(mWorkoutName);
+               }
+
+               etNameWorkout.setEnabled(false);
+
+               intent.putExtra(WorkoutService.WORKOUT_TITLE, mWorkoutName);
+               getActivity().startService(intent);
             }
-        });
+         }
+      });
 
-        return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mCircleContainer.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                mContainterWidth = mCircleContainer.getWidth();
+      bStop.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if (GPSService.running) {
+               Intent intent = new Intent(getActivity().getApplicationContext(), GPSService.class);
+               getActivity().stopService(intent);
+               bStart.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
             }
-        });
-        if (mBroadcastReceiver == null) {
-            IntentFilter filter = new IntentFilter(AppController.BROADCAST_NEW_GPS_POSITION);
+            Intent intent = new Intent(getActivity().getApplicationContext(), WorkoutService.class);
+            getActivity().stopService(intent);
+            //when the Workout is stopped, the user can't click stop button again
+            bStop.setImageDrawable(getResources().getDrawable(R.drawable.ic_stopgray));
+            mWorkoutRunning = false;
+            mWorkoutPaused = false;
+            etNameWorkout.setEnabled(true);
+            Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Workout " + mWorkoutName + " saved.", Toast.LENGTH_SHORT);
+            toast.show();
+         }
+      });
 
-            mBroadcastReceiver = new ReceiverHelper() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    GpsPoint point = intent.getParcelableExtra(GpsPoint.QUADDU_GPS_POINT);
+      return view;
+   }
 
-                    setInstantSpeed(point.getSpeed(), point.isSpeedCalculated());
-                }
-            };
-            getActivity().registerReceiver(mBroadcastReceiver, filter);
+   @Override
+   public void onStart() {
+      super.onStart();
+      mCircleContainer.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+         @Override
+         public void onGlobalLayout() {
+            mContainterWidth = mCircleContainer.getWidth();
+         }
+      });
+      if (mBroadcastReceiver == null) {
+         IntentFilter filter = new IntentFilter(AppController.BROADCAST_NEW_GPS_POSITION);
 
-        }
+         mBroadcastReceiver = new ReceiverHelper() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+               GpsPoint point = intent.getParcelableExtra(GpsPoint.QUADDU_GPS_POINT);
+
+               setInstantSpeed(point.getSpeed(), point.isSpeedCalculated());
+            }
+         };
+         getActivity().registerReceiver(mBroadcastReceiver, filter);
+
+      }
 
 /*
       new Thread(new Runnable() {
@@ -264,15 +260,15 @@ public class Workout extends Fragment {
       }).start();
 
 */
-    }
+   }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
+   @Override
+   public void onAttach(Context context) {
+      super.onAttach(context);
+   }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
+   @Override
+   public void onDetach() {
+      super.onDetach();
+   }
 }
--- app/src/main/java/so2/unica/qaddu/services/WorkoutService.java
+++ app/src/main/java/so2/unica/qaddu/services/WorkoutService.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Location;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -35,6 +36,9 @@ public class WorkoutService extends Service {
 
    BroadcastReceiver mBroadcastReceiver;
 
+   private int mIntevalLength;
+   private long mTotalTime;
+
 
    @Nullable
    @Override
@@ -51,6 +55,10 @@ public class WorkoutService extends Service {
       mItem.setName(intent.getStringExtra(WORKOUT_TITLE));
       DatabaseHelper.getIstance().addData(mItem, WorkoutItem.class);
       mPoints = new ArrayList<>();
+
+      //start the gps service
+      Intent gpsIntent = new Intent(getApplicationContext(), GPSService.class);
+      startService(gpsIntent);
 
 
       IntentFilter filter = new IntentFilter(AppController.BROADCAST_NEW_GPS_POSITION);
@@ -69,27 +77,21 @@ public class WorkoutService extends Service {
       return super.onStartCommand(intent, flags, startId);
    }
 
-   public Double calculateNewDistance(Double oldLat, Double oldLon, Double lat, Double lon) {
-      double earthRadius = 6371000; //meters
-      double dLat = Math.toRadians(oldLat - lat);
-      double dLng = Math.toRadians(oldLon - lon);
-      double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(Math.toRadians(lat)) * Math.cos(Math.toRadians(oldLat)) *
-                  Math.sin(dLng / 2) * Math.sin(dLng / 2);
-      double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-      return (earthRadius * c);
-   }
-
    public void onReceivePoint(GpsPoint point) {
       if (mPoints.size() == 0) {
          mDistance = 0.0;
       } else {
-         mDistance += calculateNewDistance(mPoints.get(mPoints.size() - 1).getLatitude(),
+         float[] distanceArray = new float[1];
+
+         Location.distanceBetween(mPoints.get(mPoints.size() - 1).getLatitude(),
                mPoints.get(mPoints.size() - 1).getLongitude(),
                point.getLatitude(),
-               point.getLongitude());
+               point.getLongitude(),
+               distanceArray);
+
+         mDistance += distanceArray[0];
       }
+
 
       mPoints.add(new WorkoutPoint(mItem, point.getLatitude(), point.getLongitude(), point.getSpeed(), point.getAltitude(), System.currentTimeMillis() - mItem.getStartDate().getTime(), mDistance));
       mItem.setTotalTime(System.currentTimeMillis() - mItem.getStartDate().getTime());
@@ -116,4 +118,86 @@ public class WorkoutService extends Service {
       sendBroadcast(intent);
       super.onDestroy();
    }
+
+   public void setIntervalLength(int intervalLength) {
+      this.mIntevalLength = intervalLength;
+   }
+
+   public double getSpeed() {
+      WorkoutPoint lastWorkoutPoint = mPoints.get(mPoints.size() - 1);
+      return lastWorkoutPoint.getSpeed();
+   }
+
+   public double getTotalSpeed() {
+
+      return msTokmh(mDistance / mTotalTime);
+   }
+
+   public double getIntevalSpeed() {
+      //fetch the workout point in the last *interval* meters
+      ArrayList<WorkoutPoint> lastPoints = new ArrayList<>();
+
+      for (int i = mPoints.size() - 1; i >= 0; i--) {
+         WorkoutPoint point = mPoints.get(i);
+         if (this.mDistance - point.getDistance() <= mIntevalLength) {
+            lastPoints.add(point);
+         }
+      }
+
+      //calculate the average speed in the last points
+      double speedSum = 0;
+
+      for (WorkoutPoint point : lastPoints) {
+         speedSum += point.getSpeed();
+      }
+
+      double averageSpeed = speedSum / lastPoints.size();
+      return msTokmh(averageSpeed);
+   }
+
+   public double getTotalStep() {
+      double timeKm = mTotalTime / (mDistance / 1000);
+
+      return timeKm;
+   }
+
+   public double getIntervalStep() {
+      //fetch the workout point in the last *interval* meters
+      ArrayList<WorkoutPoint> lastPoints = new ArrayList<>();
+
+      for (int i = mPoints.size() - 1; i >= 0; i--) {
+         WorkoutPoint point = mPoints.get(i);
+         if (this.mDistance - point.getDistance() <= mIntevalLength) {
+            lastPoints.add(point);
+         }
+      }
+
+      //calculate the average speed in the last points
+      double speedSum = 0;
+
+      for (WorkoutPoint point : lastPoints) {
+         speedSum += point.getSpeed();
+      }
+
+      double averageSpeed = speedSum / lastPoints.size();
+
+      //convert the speed from m/s to s/km
+
+      double sToKm = Math.pow(averageSpeed, -1) / 1000;
+
+      return sToKm;
+   }
+
+   public double getDistance() {
+      return mDistance / 1000;
+   }
+
+   public long getTime() {
+      return mTotalTime;
+   }
+
+   private double msTokmh(double ms) {
+      return ms * 3.6; //3.6 is the conversion factor from m/s to km/h
+   }
+
 }
