876f8973d79e76c9b029b810cd60e878df57dfa2
Stefano Lande <landeste@gmail.com>
2016-02-23 16:05:13 +0100

    fixed average pace in workout detail

--- app/src/main/java/so2/unica/qaddu/WorkoutDetailActivity.java
+++ app/src/main/java/so2/unica/qaddu/WorkoutDetailActivity.java
@@ -2,8 +2,6 @@ package so2.unica.qaddu;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,7 +11,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -36,7 +33,6 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.TimeZone;
 
 import butterknife.Bind;
@@ -216,7 +212,7 @@ public class WorkoutDetailActivity extends AppCompatActivity {
 
       simpleDateFormat = new SimpleDateFormat("mm:ss");
       simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-      String avgPace = simpleDateFormat.format(mItem.getAveragePaceInSeconds()) + " MIN/KM";
+      String avgPace = simpleDateFormat.format(mItem.getAveragePaceInSeconds() * 1000) + " MIN/KM";
       Log.d("AVGPace", mItem.getAveragePaceInSeconds() + "");
       mTvWorkoutAvgPace.setText(avgPace);
 
